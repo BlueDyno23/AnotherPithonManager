e7ecd494712622d5022d44d65aa002c8fcd1c37c
BlueDyno23 <77930454+BlueDyno23@users.noreply.github.com>
2023-07-09 11:29:30 +0300

    Fixed session timeout

--- Program.cs
+++ Program.cs
@@ -16,6 +16,7 @@ namespace AnotherPithonManager
             builder.Services.AddDistributedMemoryCache();
             builder.Services.AddSession(options =>
             {
+                options.IdleTimeout = TimeSpan.FromHours(8);
                 options.Cookie.HttpOnly = true;
                 options.Cookie.IsEssential = true;
             });
